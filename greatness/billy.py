dd53694ac28fbffd462b39470c96e19b94fe6d74
billyobuya <odongo.obuya@gmail.com>
2018-11-19 15:17:55 +0300

    uploading website

---
+++ greatness/billy.py
@@ -0,0 +1,31 @@
+<p>First Way to Display the local curent time using python.</p>
+#!/usr/bin/python
+import time;
+local time= time.asctime(time.localtime(time.time()))
+print "local current time:",local curent time
+
+<p>Second way to display the local time using python.</p>
+#!/usr/bin/python
+import time;
+local time=time.localtime(time.time())
+print "local current time:",local time
+
+<p>how to display clocks ticks as per a given period.
+#!/usr/bin/python
+import time;
+ticks=time.time()
+print "the number of ticks since 11:00p.m,september 25,2018:",ticks
+
+<p>How to draw a calender from a month.</p>
+#!/usr/bin/python
+import calender;
+cal=calender.month(september,25)
+print "here is the calender"
+print"cal"
+
+Error: Server Error
+The server encountered an error and could not complete your request.
+Please try again in 30 seconds.
+
+
+
---
+++ greatness/mainExp1.java
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+    <!--
+	package Day1.examples:
+	public class myFirstClass{
+	public static void main(string[]args){
+	System.out.println("Hello.!!Welcome to Java Development Platform.");
+	int x = 20;
+	int y = 40;
+	int result = x + y;
+	System.out.println("result=" +result);
+	}
+	//-->
+</script>
